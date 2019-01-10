269207dccf9f8f1a4f3098aaae8e9bc67a045783
Kristina <AMashKos@yandex.by>
2019-01-10 20:08:32 +0300

    new input outline, add dialog array, rename "Unicornogram".

--- ClassWork/JS/Telegram.js
+++ ClassWork/JS/Telegram.js
@@ -22,12 +22,22 @@ function createCardMessage(img, name) {
 }
 var img;
 var nameArr = ["Kristy", "Masha", 'Kate', 'Jack', 'Jone', 'Nick', 'Vika', 'Anna', 'Dasha', 'Tom'];
-
+var textArr = ['Hello. I’m calling from McCarthy’s. I helped you when you were here looking at watches the other day.',
+                'I think I have found something of yours. Have you lost anything?',
+                'Not that I have noticed. No, nothing seems to be missing.',
+                'Well, you left your credit card on one of the counters at our store.',
+                'Really? Oh, my goodness. I thought it’s been stolen. I’m so much grateful to you for calling me.',
+                'Thank you. It happens quite often. How can I return your credit card?',
+                'I’ll stop by your store tonight. What time do you work tonight at?',
+                'Let’s see. Today is Monday. I’m working the night shift. That’s till 11 pm.',
+                'Thanks Heaven! I’ll be definitely in your store tonight. I do appreciate you unimaginable' +
+                ' care for the shoppers of your store.To be on the safe side, you’d better be there at 10 am. ' +
+                'Do you know exactly when you would like to fly to Chicago? The flights are every Tuesday,' +
+                ' Thursday and Sunday.',
+                'You are welcome. Come at any convenient for you time.'];
 
 for (var i = 0; i<10; i++)
 {   img = '../HW2/Image/'+(i+1)+'.jpg';
     createCardPerson( img, nameArr[i]);
-    createCardMessage( img, 'There are a lot of books on history, about animals, travellers, children,' +
-        'school and so on. Children like to read adventure and magic books, science fiction and detective' +
-        'stories. They enjoy stories, short stories, novels, fairy-tales, fables and poems.')
+    createCardMessage( img, textArr[i]);
 }
