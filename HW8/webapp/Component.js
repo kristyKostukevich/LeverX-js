d5a07f8c12f02037f2ec5b2fc0f230d66fdedf1b
Kristina <AMashKos@yandex.by>
2019-01-23 00:32:24 +0300

    step 19

--- HW8/webapp/Component.js
+++ HW8/webapp/Component.js
@@ -1,36 +1,38 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
+    "./controllers/HelloDialog"
+
+], function (UIComponent, JSONModel, HelloDialog) {
     "use strict";
-
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-
-        metadata: {
-
-            manifest: "json"
+        metadata : {
+            manifest : "json"
         },
-
-        init: function () {
+        init : function () {
             // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
-
             // set data model
             var oData = {
-                recipient: {
-                    name: "World"
+                recipient : {
+                    name : "World"
                 }
             };
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
 
-            // set i18n model
-            var i18nModel = new ResourceModel({
-                bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
-            });
-            this.setModel(i18nModel, "i18n");
+            // set dialog
+            this._helloDialog = new HelloDialog(this.getRootControl());
+        },
+
+
+        exit : function() {
+            this._helloDialog.destroy();
+            delete this._helloDialog;
+        },
+
+        openHelloDialog : function () {
+            this._helloDialog.open();
         }
     });
-
 });
